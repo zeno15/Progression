4c68b31367001dbfad8bda9595abffcad4540624
= <markdavison0@gmail.com>
2017-03-02 21:13:13 +1300

    Removed extra namespace in tests

--- test/ECS/ComponentTests.cpp
+++ test/ECS/ComponentTests.cpp
@@ -5,14 +5,12 @@
 #include <utility>
 
 
-namespace ECS {
-    namespace tests {
-        TEST_CASE("Test default constructor applies entity id", "[Component]") {
-        	unsigned int id = 552;
+namespace tests {
+    TEST_CASE("Test default constructor applies entity id", "[Component]") {
+    	unsigned int id = 552;
 
-        	auto& component = ECS::Component(id);
+    	auto& component = ECS::Component(id);
 
-        	REQUIRE(component.getEntityId() == id);
-        } 
-	}
+    	REQUIRE(component.getEntityId() == id);
+    } 
 }
--- test/ECS/EntityTests.cpp
+++ test/ECS/EntityTests.cpp
@@ -4,25 +4,22 @@
 
 #include <utility>
 
-namespace ECS {
-    namespace tests {
-        TEST_CASE("Test default constructor applies id", "[Entity]") {
-        	unsigned int id = 552;
+namespace tests {
+    TEST_CASE("Test default constructor applies id", "[Entity]") {
+    	unsigned int id = 552;
 
-        	auto& entity = ECS::Entity(id);
+    	auto& entity = ECS::Entity(id);
 
-        	REQUIRE(entity.getId() == id);
-        }    
+    	REQUIRE(entity.getId() == id);
+    }    
 
-        TEST_CASE("Test copy constructor applies id", "[Entity]") {
-        	unsigned int id = 552;
+    TEST_CASE("Test copy constructor applies id", "[Entity]") {
+    	unsigned int id = 552;
 
-        	auto& entity1 = ECS::Entity(id);
-        	auto& entity2 = std::move(entity1);
+    	auto& entity1 = ECS::Entity(id);
+    	auto& entity2 = std::move(entity1);
 
-        	REQUIRE(entity1.getId() == id);
-        	REQUIRE(entity2.getId() == id);
-        }
+    	REQUIRE(entity1.getId() == id);
+    	REQUIRE(entity2.getId() == id);
     }
 }
-
