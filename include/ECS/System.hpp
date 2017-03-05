73ff11098589991bab1b4d9450e0da35c3b080d0
= <markdavison0@gmail.com>
2017-03-05 18:08:56 +1300

    Added systems

---
+++ include/ECS/System.hpp
@@ -0,0 +1,151 @@
+#pragma once
+
+#include <ECS/Entity.hpp>
+
+#include <cassert>
+#include <algorithm> 
+
+//#include <SFML/Graphics.hpp>
+
+////////////////////////////////////////////////////////////
+///
+/// \namespace  ECS
+///
+////////////////////////////////////////////////////////////
+namespace ECS {
+
+    ////////////////////////////////////////////////////////////
+    ///
+    /// \brief  Base class for ECS System, allows vectors of systems
+    ///
+    ////////////////////////////////////////////////////////////
+    class SystemBase {
+    public:
+        ////////////////////////////////////////////////////////////
+        ///
+        /// \brief  Virtual destructor
+        ///
+        ////////////////////////////////////////////////////////////
+        virtual ~SystemBase() {};
+
+        ////////////////////////////////////////////////////////////
+        ///
+        /// \brief  Updates all entities within the system
+        ///
+        /// \param  The elapsed update time in seconds.
+        ///
+        ////////////////////////////////////////////////////////////
+        virtual void update(float _delta) {};
+        ////////////////////////////////////////////////////////////
+        ///
+        /// \brief  Passes events to all enties within the system.
+        ///
+        /// \param  The system event to process
+        ///
+        ////////////////////////////////////////////////////////////
+        virtual void handleEvent(const sf::Event& _event) {};
+        ////////////////////////////////////////////////////////////
+        ///
+        /// \brief  Draws all entities within the system.
+        ///
+        /// \param  The render _target
+        ///
+        /// \param  The render _states
+        ///
+        ////////////////////////////////////////////////////////////
+        //virtual void draw(sf::RenderTarget& _target, sf::RenderStates _states) const {};
+
+        ////////////////////////////////////////////////////////////
+        ///
+        /// \brief  Adds an entity to the system.
+        ///
+        /// \param  The entity to be added.
+        ///
+        ////////////////////////////////////////////////////////////
+        virtual void addEntity(Entity& _entity) = 0;
+        ////////////////////////////////////////////////////////////
+        ///
+        /// \brief  Checks whether an entity is within the system
+        ///
+        /// \param  The entity id
+        ///
+        /// \return Whether the entity id is within the system
+        ///
+        ////////////////////////////////////////////////////////////
+        virtual bool containsEntity(unsigned int _id) = 0;
+        ////////////////////////////////////////////////////////////
+        ///
+        /// \brief  Removes an entity from the system
+        ///
+        /// \param  The entity id to remove.
+        ///
+        ////////////////////////////////////////////////////////////
+        virtual void removeEnitity(unsigned int _id) = 0;
+    protected:
+        std::vector<Entity *> m_SystemEntities;         ///<    Entities that belong to this system
+    };
+
+    ////////////////////////////////////////////////////////////
+    ///
+    /// \brief  Base for all ECS systems
+    ///
+    /// \tparam The component types the system requires.
+    ///
+    ////////////////////////////////////////////////////////////
+    template <typename... Args>
+    class System : public SystemBase {
+    public:
+        ////////////////////////////////////////////////////////////
+        ///
+        /// \brief  Virtual destructor
+        ///
+        ////////////////////////////////////////////////////////////
+        virtual ~System() {};
+
+
+        ////////////////////////////////////////////////////////////
+        ///
+        /// \brief  Adds an entity to the system.
+        ///
+        /// \param  The entity to be added.
+        ///
+        ////////////////////////////////////////////////////////////
+        virtual void addEntity(Entity& _entity) override {
+            assert(_entity.hasComponents<Args...>());
+            assert(!containsEntity(_entity.getId()));
+
+            m_SystemEntities.push_back(&_entity);
+        }
+
+        ////////////////////////////////////////////////////////////
+        ///
+        /// \brief  Checks whether an entity is within the system
+        ///
+        /// \param  The entity id
+        ///
+        /// \return Whether the entity id is within the system
+        ///
+        ////////////////////////////////////////////////////////////
+        virtual bool containsEntity(unsigned int _id) override {
+            return m_SystemEntities.end() != std::find_if(m_SystemEntities.begin(), m_SystemEntities.end(), [&](const Entity *_entity) {
+                return _entity->getId() == _id;
+            });
+        }
+
+        ////////////////////////////////////////////////////////////
+        ///
+        /// \brief  Removes an entity from the system
+        ///
+        /// \param  The entity id to remove.
+        ///
+        ////////////////////////////////////////////////////////////
+        virtual void removeEnitity(unsigned int _id) override {
+            m_SystemEntities.erase(
+                std::remove_if(m_SystemEntities.begin(), m_SystemEntities.end(), [&](const Entity *_entity) {
+                    return _entity->getId() == _id;
+                }), m_SystemEntities.end());
+        }
+
+    };
+
+}
