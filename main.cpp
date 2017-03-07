8029de03a6f8d9bf87bf96a9957b9136d526883e
= <markdavison0@gmail.com>
2017-03-07 18:30:26 +1300

    Adding test of sfml path stuff

--- main.cpp
+++ main.cpp
@@ -7,10 +7,14 @@
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
+#include <SFML/Graphics.hpp>
 
 
 int main( const int _argc, const char **_argv ) {
-	std::cout << "Hello World!" << std::endl;
+	sf::Vector2f vec;
+	vec.x = 5.2f;
+	vec.y = 21.2f;
+	std::cout << "Hello World! x: " << vec.x << ", y: " << vec.y << std::endl;
 
 	return EXIT_SUCCESS;
 }
