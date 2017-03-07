da6fd0bda9d8984eb90f7c1e3a80ee93242df374
= <markdavison0@gmail.com>
2017-03-07 18:13:31 +1300

    Now unpacking and installing/moving

--- scripts/install_sfml.sh
+++ scripts/install_sfml.sh
@@ -6,14 +6,15 @@ set -ev
 if [[ $CXX = "clang++" ]]; then
 	echo "CLANG"
 	wget http://www.sfml-dev.org/files/SFML-2.4.2-osx-clang.tar.gz --no-check-certificate
-	#tar -xzvf SFML-2.4.2-osx-clang.tar.gz
-	#mv SFML-2.4.2-osx-clang $HOME/SFML
+	tar -xzvf SFML-2.4.2-osx-clang.tar.gz
+	mv SFML-2.4.2-osx-clang $HOME/SFML
 
 elif [[ $CXX = "g++" ]]; then
 	echo "G++"
 	wget http://www.sfml-dev.org/files/SFML-2.4.2-linux-gcc-64-bit.tar.gz --no-check-certificate
-	#tar -xzvf SFML-2.4.2-linux-gcc-64-bit.tar.gz
-	#mv SFML-2.4.2-linux-gcc-64-bit $HOME/SFML
-
+	tar -xzvf SFML-2.4.2-linux-gcc-64-bit.tar.gz
+	mv SFML-2.4.2-linux-gcc-64-bit $HOME/SFML
 
 fi
+
+ls $HOME
